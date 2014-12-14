e22bda260bc049d1ecb93af38f7aaf4fa2d6ddcb
NemoChenTW <nemochentw@gmail.com>
2014-12-14 20:31:07 +0800

    Add script autoGen & autoRemove.

---
+++ autoRemove.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+rm aclocal.m4
+rm -r autom4te.cache
+rm compile
+rm config.h
+rm config.h.in
+rm config.log
+rm config.status
+rm configure
+rm depcomp
+rm install-sh
+rm Makefile
+rm Makefile.in
+rm missing
+rm stamp-h1
